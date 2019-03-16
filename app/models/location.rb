27e240c94c2c5dd52057c837f18c62c8f1e3f207
Muhammad Abdullah Shafiq <iamabdullahshafiq@gmail.com>
2019-03-16 14:48:07 +0500

    Annotate: Generated Schema information using annotate gem

--- app/models/location.rb
+++ app/models/location.rb
@@ -1,2 +1,15 @@
+# == Schema Information
+#
+# Table name: locations
+#
+#  id          :integer          not null, primary key
+#  title       :string
+#  description :text
+#  latitude    :float
+#  longitude   :float
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Location < ApplicationRecord
 end
