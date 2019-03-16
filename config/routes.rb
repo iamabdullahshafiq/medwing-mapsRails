95d48049bb7e3a93396e841532d8a7b6147f3ff0
Muhammad Abdullah Shafiq <iamabdullahshafiq@gmail.com>
2019-03-16 15:17:05 +0500

    Location: Added namespacing for controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :locations
+  namespace :v1 do
+    resources :locations
+  end
 end
